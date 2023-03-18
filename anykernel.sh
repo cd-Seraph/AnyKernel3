6460444100c1124662c71cf3b41285bfb4b34e85
cd-Seraph <120856294+cd-Seraph@users.noreply.github.com>
2023-03-18 17:08:29 +0800

    anykernel: void

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Feather Kernel by Seraph
+kernel.string=Void Kernel by Seraph
 do.devicecheck=1
 do.modules=1
 do.systemless=1
